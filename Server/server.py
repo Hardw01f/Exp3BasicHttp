d5e3f998011ddb2503e66a288da4904ed49e91af
kanon <yuriika.0000@gmail.com>
2020-06-03 17:03:52 +0900

    fix server.py

--- Server/server.py
+++ Server/server.py
@@ -25,33 +25,6 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-Lengt', '13')
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
             self.end_headers()
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import parse_qs, urlparse
-import requests
-import json
-import socket
-
-address = ('127.0.0.1', 9000)
-
-
-class MyHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print('path = {}'.format(self.path))
-
-
-        parsed_path = urlparse(self.path)
-        print('parsed: path = {}, query = {}'.format(parsed_path.path, parse_qs(parsed_path.query)))
-
-        print('headers\r\n-----\r\n{}-----'.format(self.headers))
-        host = socket.gethostname()
-        ip = socket.gethostbyname(host)
-        if parsed_path.path == "/hi":
-
-            self.send_response(200)
-            self.send_header('Hello','BasicHTTP!')
-            self.send_header('Content-Lengt', '13')
-            self.send_header('Content-Type', 'text/plain; charset=utf-8')
-            self.end_headers()
 
 
             user_address = ip.encode()
